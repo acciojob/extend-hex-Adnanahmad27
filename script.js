eeb2194fddca0d0546ae15b530a15b8819d214ed
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-16 18:35:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  //write your code here
+	let res = '';
+	let i = 0;
+	if(shortHex.length === 4){
+		i++;
+	}
+	while (i < shortHex.length) {
+		let char = shortHex.charAt(i);
+		res += char + char;
+		i++;
+	}
+	return res;
 };
 
 // Do not change the code below.
